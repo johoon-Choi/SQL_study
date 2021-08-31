3a8bdd6f7886be88f60285c820551106a9bec97c
hoondabal <74132025+johoon-Choi@users.noreply.github.com>
2021-08-31 19:09:07 +0900

    Leetcode
    #SUB_QUERY

--- Leetcode/Easy/176. Second Highest Salary.sql
+++ Leetcode/Easy/176. Second Highest Salary.sql
@@ -1,4 +1,5 @@
-SELECT MAX(Salary) AS SecondHighestSalary FROM Employee
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
 WHERE Salary < (SELECT MAX(Salary)
                 FROM Employee);
                

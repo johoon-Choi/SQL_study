17714ec541c3c747a7723be77584f05cdbe9efac
hoondabal <74132025+johoon-Choi@users.noreply.github.com>
2021-09-03 22:52:57 +0900

    Update 180. Consecutive Number.sql

--- Leetcode/Medium/180. Consecutive Number.sql
+++ Leetcode/Medium/180. Consecutive Number.sql
@@ -1,9 +1,10 @@
+# JOIN을 활용한 풀이
 SELECT DISTINCT A.Num AS ConsecutiveNums
 FROM Logs AS A
     JOIN Logs AS B ON A.Id + 1 = B.Id AND A.Num = B.Num
     JOIN Logs AS C ON A.Id + 2 = C.Id AND A.Num = C.num;
     
-
+# SUB QUERY OF SUB QUERY를 활용한 
 SELECT DISTINCT A.Num AS ConsecutiveNums
 FROM Logs AS A
 WHERE A.Num = (

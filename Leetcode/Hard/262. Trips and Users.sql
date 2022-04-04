b0df5b5a3c963e0a40f84096045f38cb7b8c9ac3
JOHOONDAY <74132025+johoon-Choi@users.noreply.github.com>
2022-04-04 17:52:15 +0900

    Update 262. Trips and Users.sql

--- Leetcode/Hard/262. Trips and Users.sql
+++ Leetcode/Hard/262. Trips and Users.sql
@@ -1,15 +1,15 @@
-SELECT T1.request_at AS 'Day', 
-       ROUND(COUNT(IF(T1.status != 'completed', 1, NULL))/COUNT(*), 2) 
+SELECT T.request_at AS 'Day', 
+       ROUND(COUNT(IF(T.status != 'completed', 1, NULL))/COUNT(*), 2) 
             AS 'Cancellation Rate'
-FROM Trips AS T1
-WHERE T1.client_id NOT IN (
-                            SELECT users_id FROM Users
-                            WHERE banned = 'Yes'
-                          )
+FROM Trips AS T
+WHERE T.client_id NOT IN (
+                          SELECT users_id FROM Users
+                          WHERE banned = 'Yes'
+                         )
                           AND
-      T1.driver_id NOT IN (
-                            SELECT users_id FROM Users
-                            WHERE banned = 'Yes'
-                          )   
+      T.driver_id NOT IN (
+                          SELECT users_id FROM Users
+                          WHERE banned = 'Yes'
+                         )   
 GROUP BY request_at
-HAVING T1.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+HAVING T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
